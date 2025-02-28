d054d391c26f72d6369b1f0e4a9c17351443a3f0
suresh <suresh.yadav@vanshiv.com>
2025-02-28 14:36:20 +0530

    added comments under WeeklyTimesheetController.php

--- app/Http/Controllers/WeeklyTimesheetController.php
+++ app/Http/Controllers/WeeklyTimesheetController.php
@@ -211,7 +211,7 @@ class WeeklyTimesheetController extends AccountBaseController
 
 
 
-    // new code
+    // new code (added a logic to views the memo in timesheet when click on view )
     public function store(Request $request)
         {
             $taskIds = $request->task_ids;
